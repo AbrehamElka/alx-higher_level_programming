58bb4ab1d3eb38165a56030c47bff4d699e42be9
Abreham Dessalegn <abrehamdessalegn02@gmail.com>
2024-03-11 18:24:42 +0300

    task 6

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_fst_3 = word[:3]
+word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
 print("Fst 3 letters: {}".format(word_fst_3))
