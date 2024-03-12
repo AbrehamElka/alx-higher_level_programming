f4c88b4f5850327b1fb049a089fca7de2fc8e4d6
Abreham <abrehamdessalegn02@gmail.com>
2024-03-12 13:02:02 +0000

    task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def fizzbuzz():
-	for i in range(101):
+	for i in range(1, 101):
 		if i % 3 == 0 and i % 5 == 0:
 			print("FizzBuzz ", end="")
 			continue
@@ -10,4 +11,3 @@ def fizzbuzz():
 			print("Buzz ", end="")
 		else:
 			print("{} ".format(i), end="")
-	print("$", end="")
