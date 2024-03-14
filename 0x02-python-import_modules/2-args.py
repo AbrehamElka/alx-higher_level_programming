348fefb298f010dd8227565728b82e061d2f0045
Abreham <abrehamdessalegn02@gmail.com>
2024-03-14 16:50:43 +0000

    task 2 string edited

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,13 @@ if __name__ == "__main__":
 	from sys import argv
 	a = len(argv)
 	b = 0
-	if a == 2:
-		print("1 argument\n1: {}".format(argv[a - 1]))
+	if a == 1:
+		print("0 arguments.")
 	else:
-		print("{} arguments".format(a - 1))
+		if a > 2:
+			print("{} arguments:".format(a - 1))
+		else:
+			print("1 argument:")
 		while(a > 1):
 			print("{}: {}".format(b + 1, argv[b + 1]))
 			b += 1
