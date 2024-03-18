9f8093b1ec23614a0112acdb9badb5d470ea6386
Abreham <abrehamdessalegn02@gmail.com>
2024-03-18 08:20:38 +0000

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+	x = len(my_list)
+	y = 0
+	new_l = []
+
+	while ((y + 1) != x):
+		if my_list[y] % 2 == 0:
+			new_l.append(True)
+		else:
+			new_l.append(False)
+		y += 1
+	return (new_l)
