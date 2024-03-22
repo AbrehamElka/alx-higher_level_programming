88522df487487c65eb4335c2f58aea6f447201bd
AbrehamElka <abrehamdessalegn02@gmail.com>
2024-03-22 07:41:17 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,26 +2,15 @@
 
 def roman_to_int(roman_string):
 	dict_r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-	index = 0
-	jump = False
 	tot = 0
-	temp = 0
-	len_str = len(roman_string)
-
+	val = 0
+	prev = 0
 	for i in roman_string:
-		if index + 1 < len_str:
-			if dict_r[i] < dict_r[roman_string[index + 1]] or jump:
-				if jump:
-					index += 1
-					jump = False
-					continue
-				else:
-					jump = True
-				temp = dict_r[roman_string[index + 1]] - dict_r[i]
-				tot += temp
-				index += 1
-				continue
-		tot += dict_r[i]
-		index += 1
-
+		val = dict_r[i]
+		if val > prev:
+			tot = tot - prev
+			tot = tot + val - prev
+		else:
+			tot += val
+		prev = val
 	return (tot)
