cd1f90a44ace7059cda9fe0b4347e83f561cd5bf
AbrehamElka <abrehamdessalegn02@gmail.com>
2024-05-10 10:30:11 +0300

    static method

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+private setters
+"""
+
+
+class Square:
+    """
+    A square class.
+    """
+    def __init__(self, size=0):
+        self.__size = self.set_size(self, size)
+
+    @staticmethod
+    def set_size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        return value
