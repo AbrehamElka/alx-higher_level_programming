2bb6a02f648272574abd6b248277042480c534ef
AbrehamElka <abrehamdessalegn02@gmail.com>
2024-05-10 17:09:15 +0300

    code edit for edge cases

--- 0x06-python-classes/6-main.py
+++ 0x06-python-classes/6-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 Square = __import__('6-square').Square
 
-my_square = Square(0, (10, 3))
-my_square.my_print()
+try:
+    my_square = Square(3, (1, ))
+except Exception as e:
+    print(e)
