9cde2cd2268e05bf3491d964f084b5431f1fabd9
AbrehamElka <abrehamdessalegn02@gmail.com>
2024-05-10 16:13:26 +0300

    square printer

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Properties of a class.
+"""
+
+
+class Square:
+    """
+    A square class.
+    """
+    def __init__(self, size=0):
+        self.__size = self.set_size(self, size)
+
+    @staticmethod
+    def set_size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        return value
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = self.set_size(self, value)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            pass
+        for i in range(0, self.size):
+            for i in range(0, self.size):
+                print("#", end="")
+            print()
